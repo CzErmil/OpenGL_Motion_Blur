07f3b4251131effde9e54ba30a3dc73d48696d44
Emil Czarnocki <emilczarnoc@gmail.com>
2022-02-07 19:54:30 +0100

    Modified texture constructor

--- OpenGL_Motion_Blur/src/Texture.cpp
+++ OpenGL_Motion_Blur/src/Texture.cpp
@@ -60,18 +60,21 @@ Texture::Texture(const std::string& fileName, int parm)
 		stbi_image_free(m_LocalBuffer);
 }
 
-Texture::Texture(int width, int height, int param, int internalFormat,int format, int type)
-	: m_ID(0), m_FileName(""), m_Slot(0), m_Width(width), m_Height(height), m_BytesPerPixel(0), m_LocalBuffer(nullptr)
+Texture::Texture(int width, int height, int param, int internalFormat, int format, int type)
+	: Texture(GL_TEXTURE_2D, 1, m_Width, m_Width, param, internalFormat, format, type) {}
+
+Texture::Texture(int target,unsigned int count, int width, int height, int param, int internalFormat, int format, int type)
+	: m_ID(0), m_FileName(""), m_Slot(0), m_Count(count), m_Width(width), m_Height(height), m_BytesPerPixel(0), m_LocalBuffer(nullptr)
 {
-	glGenTextures(1, &m_ID);
-	glBindTexture(GL_TEXTURE_2D, m_ID);
+	glGenTextures(m_Count, &m_ID);
+	glBindTexture(target, m_ID);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, param);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, param);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+	glTexParameteri(target, GL_TEXTURE_MIN_FILTER, param);
+	glTexParameteri(target, GL_TEXTURE_MAG_FILTER, param);
+	glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 
-	glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, m_Width, m_Height, 0, format, type, m_LocalBuffer);
+	glTexImage2D(target, 0, internalFormat, m_Width, m_Height, 0, format, type, m_LocalBuffer);
 }
 
 Texture::~Texture()
--- OpenGL_Motion_Blur/src/Texture.h
+++ OpenGL_Motion_Blur/src/Texture.h
@@ -8,6 +8,7 @@ private:
 	GLuint m_ID;
 	std::string m_FileName;
 	GLuint m_Slot;
+	GLuint m_Count;
 	int m_Width;
 	int m_Height;
 	int m_BytesPerPixel;
@@ -16,12 +17,14 @@ public:
 	Texture(const std::string& fileName);
 	Texture(const std::string& fileName, int parm);
 	Texture(int width, int height, int param, int internalFormat, int format, int type);
+	Texture(int target, unsigned int count, int width, int height, int param, int internalFormat, int format, int type);
 	~Texture();
 
 	void Bind(unsigned int slot = 0);
 	void Unbind();
 
 	inline unsigned int GetID() const { return m_ID; };
+	inline unsigned int GetCount() const{ return m_Count; };
 	inline int GetWidth() const { return m_Width; };
 	inline int GetHeight() const { return m_Height; };
 	inline int GetSlot() const { return m_Slot; };
