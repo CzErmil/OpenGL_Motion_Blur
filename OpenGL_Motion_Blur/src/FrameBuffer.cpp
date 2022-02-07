56b5f0cea3c47d70daf5a27ccc441cd266b25932
Emil Czarnocki <emilczarnoc@gmail.com>
2022-02-07 20:34:27 +0100

    Added the ability to attach different types of textures to the Framebuffer

--- OpenGL_Motion_Blur/src/FrameBuffer.cpp
+++ OpenGL_Motion_Blur/src/FrameBuffer.cpp
@@ -1,37 +1,42 @@
 #include "FrameBuffer.h"
 
 FrameBuffer::FrameBuffer(Attachment attachment, int width, int height) :
-    m_Width(width), m_Height(height)
+	FrameBuffer(attachment, 1, width, height) {}
+
+FrameBuffer::FrameBuffer(Attachment attachment, unsigned int textureCount, int width, int height) :
+	m_TextureCount(textureCount), m_Width(width), m_Height(height)
 {
-    glGenFramebuffers(1, &m_ID);
+	glGenFramebuffers(1, &m_ID);
 
-    Bind();
+	Bind();
 
-    switch (attachment)
-    {
-    case Attachment::COLOR_RGBA8:
-        m_Texture = std::make_unique<Texture>(m_Width, m_Height, GL_NEAREST, GL_RGBA8, GL_RGBA, GL_UNSIGNED_BYTE);
+	int target = m_TextureCount > 1 ? GL_TEXTURE_2D_ARRAY : GL_TEXTURE_2D;
 
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_Texture->GetID(), 0);
-        break;
-    case Attachment::COLOR_RGBA16F:
-        m_Texture = std::make_unique<Texture>(m_Width, m_Height, GL_NEAREST, GL_RGBA16F, GL_RGBA, GL_UNSIGNED_BYTE);
+	switch (attachment)
+	{
+	case Attachment::COLOR_RGBA8:
+		m_Texture = std::make_unique<Texture>(target, m_TextureCount, m_Width, m_Height, GL_NEAREST, GL_RGBA8, GL_RGBA, GL_UNSIGNED_BYTE);
 
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_Texture->GetID(), 0);
-        break;
-    case Attachment::DEPTH:
-        m_Texture = std::make_unique<Texture>(m_Width, m_Height, GL_NEAREST, GL_DEPTH_COMPONENT, GL_DEPTH_COMPONENT, GL_FLOAT);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_Texture->GetID(), 0);
+		break;
+	case Attachment::COLOR_RGBA16F:
+		m_Texture = std::make_unique<Texture>(target, m_TextureCount, m_Width, m_Height, GL_NEAREST, GL_RGBA16F, GL_RGBA, GL_UNSIGNED_BYTE);
 
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_Texture->GetID(), 0);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_Texture->GetID(), 0);
+		break;
+	case Attachment::DEPTH:
+		m_Texture = std::make_unique<Texture>(target, m_TextureCount, m_Width, m_Height, GL_NEAREST, GL_DEPTH_COMPONENT, GL_DEPTH_COMPONENT, GL_FLOAT);
 
-        glDrawBuffer(GL_NONE);
-        glReadBuffer(GL_NONE);
-        break;
-    default:
-        break;
-    }
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_Texture->GetID(), 0);
 
-    Unbind();
+		glDrawBuffer(GL_NONE);
+		glReadBuffer(GL_NONE);
+		break;
+	default:
+		break;
+	}
+
+	Unbind();
 }
 
 FrameBuffer::~FrameBuffer()
@@ -41,24 +46,21 @@ FrameBuffer::~FrameBuffer()
 
 void FrameBuffer::Bind()
 {
-    glBindFramebuffer(GL_FRAMEBUFFER, m_ID);
+	glBindFramebuffer(GL_FRAMEBUFFER, m_ID);
 }
 
 void FrameBuffer::Unbind()
 {
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
 
 void FrameBuffer::SetDepthBuffer()
 {
-    Bind();
-    GLuint depthrenderbuffer;
-    glGenRenderbuffers(1, &depthrenderbuffer);
-    glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, m_Width, m_Height);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
-
-    GLenum DrawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
-    glDrawBuffers(1, DrawBuffers);
-    Unbind();
+	Bind();
+	GLuint depthrenderbuffer;
+	glGenRenderbuffers(m_TextureCount, &depthrenderbuffer);
+	glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, m_Width, m_Height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
+	Unbind();
 }
--- OpenGL_Motion_Blur/src/FrameBuffer.h
+++ OpenGL_Motion_Blur/src/FrameBuffer.h
@@ -14,10 +14,12 @@ class FrameBuffer
 {
 private:
 	unsigned int m_ID;
+	unsigned int m_TextureCount;
 	int m_Width, m_Height;
 	std::unique_ptr<Texture> m_Texture;
 public:
 	FrameBuffer(Attachment attachment, int width, int height);
+	FrameBuffer(Attachment attachment, unsigned int texturCount, int width, int height);
 	~FrameBuffer();
 
 	void Bind();
